ae3b6c204032d1b2a717bf513b7cefe60e51febf
Tania Allard <t.sanchez@sheffield.ac.uk>
2017-12-12 22:16:20 +0000

    Add day 12

--- day12.py
+++ day12.py
@@ -3,11 +3,17 @@
 Advent of Code 2017: Day 10
 """
 
+import aoc
+# Getting the input for the puzzle
+helper = aoc.puzzle_data(year = 2017, day = 12)
+
+# Note that for this one I donwloaded the input and
+# worked on the train
+file = open('./inputs/17_input5.txt')
 connect = {}
-for line in open('./inputs/17_input5.txt'):
+for line in file:
     line = line.strip().split()
     connect[line[0]] = [x.strip(',') for x in line[2:]]
-
 
 seen = set()
 def find_pipes(progr):
@@ -18,6 +24,14 @@ def find_pipes(progr):
         find_pipes(x)
 
 
-find_pipes('0')
-print(seen)
-print(len(seen))
+#find_pipes('0')
+#print('Part 1: {}'.format(len(seen)))
+
+
+seen = set()
+components = 0
+while len(seen) < len(connect):
+    n = (set(connect.keys()) - seen).pop()
+    find_pipes(n)
+    components += 1
+print('B', components)
