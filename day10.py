95d0ce832821f3dc6ab75a851649d82a61711847
Tania Allard <t.sanchez@sheffield.ac.uk>
2017-12-22 16:40:16 +0000

    Add day 10

---
+++ day10.py
@@ -0,0 +1,56 @@
+"""
+@trallard
+Advent of Code 2017: Day 10
+"""
+import aoc
+
+# Getting the input for the puzzle
+helper = aoc.puzzle_data(year = 2017, day = 10)
+file = helper.get_data()
+print(file)
+
+def reverse_range(li, a, size):
+    for i in range(size // 2):
+        x = (a + i) % len(li)
+        y = (a + size - 1 - i) % len(li)
+        li[x], li[y] = li[y], li[x]
+
+
+def solveA(lengths):
+    elements = list(range(256))
+    idx = 0
+    skip = 0
+    for l in lengths:
+        reverse_range(elements, idx, l)
+        idx += l + skip
+        skip += 1
+    return elements[0] * elements[1]
+
+
+def solveB(lengths):
+    elements = list(range(256))
+    idx = 0
+    skip = 0
+    for _ in range(64):
+        for l in lengths:
+            reverse_range(elements, idx, l)
+            idx += l + skip
+            skip += 1
+    dense = []
+    for i in range(0, 256, 16):
+        c = 0
+        for j in range(i, i + 16):
+            c ^= elements[j]
+        c = hex(c).split('x')[1]
+        if len(c) == 1:
+            c = '0' + c
+        dense.append(c)
+    return ''.join(dense)
+
+
+lengths = [int(x) for x in next(open('input')).strip().split(',')]
+print('A', solveA(lengths))
+
+
+lengths = [ord(c) for c in next(open('input')).strip()] + [17, 31, 73, 47, 23]
+print('B', solveB(lengths))
